f858b84d468882a140c2165d9a5ec185027d14b0
Mark <markduan@yunify.com>
2021-08-06 16:28:04 +0800

    refactor: Update get getLocalStateStream

--- packages/render-engine/src/use-local-state.ts
+++ packages/render-engine/src/use-local-state.ts
@@ -6,20 +6,25 @@ type LocalStore<T> = Record<string, [Subject<T>, SetState<T>]>;
 
 const localStore: LocalStore<any> = {};
 
-function createLocalStateStream<T>(key: string): [Subject<T>, SetState<T>] {
+function createLocalStream<T>(): [Subject<T>, SetState<T>] {
+  const state$ = new Subject<T>();
+  function updater(state: T): void {
+    state$.next(state);
+  }
+
+  return [state$, updater];
+}
+
+function getLocalStateStream<T>(key: string): [Subject<T>, SetState<T>] {
   if (!localStore[key]) {
-    const state$ = new Subject<T>();
-    function updater(state: T) {
-      state$.next(state);
-    }
-    localStore[key] = [state$, updater];
+    localStore[key] = createLocalStream();
   }
 
   return localStore[key];
 }
 
 function useLocalState<T>(key: string, defaultState?: T): [T | undefined, SetState<T>] {
-  const [state$, updater] = createLocalStateStream<T>(key);
+  const [state$, updater] = getLocalStateStream<T>(key);
 
   return [useObservableState(state$), updater];
 }
