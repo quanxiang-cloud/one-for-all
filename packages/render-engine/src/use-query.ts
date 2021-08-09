ace467b82adc776f47d7fc69b0932f3901c980c0
Mark <markduan@yunify.com>
2021-08-09 15:29:08 +0800

    refactor: Convert api stream to Observable value

--- packages/render-engine/src/use-query.ts
+++ packages/render-engine/src/use-query.ts
@@ -1,6 +1,11 @@
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { OpenAPIV3 } from 'openapi-types';
 
-import APIStream from './api-stream';
+import APIStream, { APIResult } from './api-stream';
+import { RequestParams } from 'packages/request-builder/src';
+
+type Convertor<T> = (result: APIResult) => T;
 
 export class QueryResult {
   apiDoc: OpenAPIV3.Document;
@@ -9,5 +14,18 @@ export class QueryResult {
   constructor(apiDoc: OpenAPIV3.Document, streamIDMap: Record<string, string>) {
     this.apiDoc = apiDoc;
     this.apiStream = new APIStream(this.apiDoc, streamIDMap);
+  }
+
+  getValue<T>(streamID: string, convertor: Convertor<T>): Observable<T> {
+    const [apiStream$] = this.apiStream.getStream(streamID);
+
+    return apiStream$.pipe(map(convertor));
+  }
+
+  getAction(streamID: string, convertor: (params?: any) => RequestParams): (params: any) => void {
+    const [, { next }] = this.apiStream.getStream(streamID);
+    return (params: any) => {
+      next(convertor(params));
+    };
   }
 }
