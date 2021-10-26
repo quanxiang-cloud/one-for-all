d3089e52e278c09c089772cf798ff7eef6cc0eed
Mark <markduan@yunify.com>
2021-10-26 15:36:39 +0800

    refactor: Uncommnet logger method in utils

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -2,15 +2,14 @@ const noop = (): void => undefined;
 
 export default new Proxy(console, {
   get: function(target, propKey, receiver): any {
+    if (window.__verbose_log__) {
+      return Reflect.get(target, propKey, receiver);
+    }
+
+    if (propKey === 'warn' || propKey === 'log' || propKey === 'debug') {
+      return noop;
+    }
+
     return Reflect.get(target, propKey, receiver);
-    // if (window.__verbose_log__) {
-    //   return Reflect.get(target, propKey, receiver);
-    // }
-
-    // if (propKey === 'warn' || propKey === 'log' || propKey === 'debug') {
-    //   return noop;
-    // }
-
-    // return Reflect.get(target, propKey, receiver);
   },
 });
