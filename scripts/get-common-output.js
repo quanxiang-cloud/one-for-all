10de1e791743f945e7b734b6a92b3e756f24f1a1
Mark <markduan@yunify.com>
2021-11-03 16:41:55 +0800

    chore: Remove useless configs

--- scripts/get-common-output.js
+++ scripts/get-common-output.js
@@ -1,14 +1,14 @@
 import { terser } from 'rollup-plugin-terser';
 
-export default function getOutput(packageName) {
+export default function getOutput(pkgName) {
   return [
     {
-      file: `dist/${packageName}/index.js`,
+      file: `dist/${pkgName}/index.js`,
       format: 'system',
       sourcemap: 'inline',
     },
     {
-      file: `dist/${packageName}/index.min.js`,
+      file: `dist/${pkgName}/index.min.js`,
       format: 'system',
       sourcemap: false,
       plugins: [terser()],
