05ce0d574790e771a80802eab7780cf687241e48
Mark <markduan@yunify.com>
2021-11-08 21:40:42 +0800

    test: Update src/__tests__/fixtures/search-btn.tsx

--- packages/render-engine/src/__tests__/fixtures/search-btn.tsx
+++ packages/render-engine/src/__tests__/fixtures/search-btn.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import APIStateHub from '../../api-state-hub';
-import { APIInvokeProperty, APIDerivedProperty, Instantiated } from '../../types';
+import { APIInvokeProperty, APIDerivedProperty, Instantiated, CTX } from '../../types';
 import useConnection from '../../use-connection';
 
 type Props = {
   nodeProps: Record<string, APIInvokeProperty<Instantiated> | APIDerivedProperty<Instantiated>>;
-  stateHub: APIStateHub;
+  ctx: CTX
 }
 
-export default function Link({ nodeProps, stateHub }: Props): JSX.Element {
-  const { foo, bar, onFetch } = useConnection({ nodeProps, stateHub });
+export default function Link({ nodeProps, ctx }: Props): JSX.Element {
+  const { foo, bar, onFetch } = useConnection({ nodeProps, ctx });
 
   return (
     <button id="button" onClick={onFetch}>{`${foo}:${bar}`}</button>
