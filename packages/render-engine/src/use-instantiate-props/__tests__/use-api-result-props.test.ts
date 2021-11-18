52f144a5992195491c814b75331af7fc63a073f6
Mark <markduan@yunify.com>
2021-11-18 22:03:47 +0800

    test: Update test cases for useAPIResultProps

--- packages/render-engine/src/use-instantiate-props/__tests__/use-api-result-props.test.ts
+++ packages/render-engine/src/use-instantiate-props/__tests__/use-api-result-props.test.ts
@@ -1,5 +1,5 @@
 import mockXHR from 'xhr-mock';
-import { renderHook } from '@testing-library/react-hooks/pure';
+import { act, renderHook } from '@testing-library/react-hooks/pure';
 import type { Adapter } from '@ofa/api-spec-adapter';
 
 import useAPIResultProps from '../use-api-result-props';
@@ -14,14 +14,14 @@ const builder: Adapter = {
   build: () => ({ url: '/api', method: 'get' }),
 };
 
-describe('useAPIResultProps_resolve_expected_fallback', () => {
-  const stateIDMap = {
-    some_api_state: { apiID: 'get:/api' },
-  };
-  const apiStateHub = new APIStateHub(builder, stateIDMap);
-  dummyCTX.apiStates = apiStateHub;
+const stateIDMap = {
+  some_api_state: { apiID: 'get:/api' },
+};
 
+describe('useAPIResultProps_resolve_expected_fallback', () => {
   test('when_it_is_the_initial_state', () => {
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
     const fallback = { foo: 123 };
     const node: SchemaNode<Instantiated> = {
       key: 'some_key',
@@ -37,30 +37,129 @@ describe('useAPIResultProps_resolve_expected_fallback', () => {
     };
 
     const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
-    expect(result.current.foo).toMatchObject(fallback);
+    expect(result.current.foo).toBe(fallback);
 
     unmount();
   });
 
-  // test('when_api_data_is_undefined', () => {
+  test('when_api_return_error', () => {
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
+    const fallback = { foo: 123 };
+    const node: SchemaNode<Instantiated> = {
+      key: 'some_key',
+      type: 'html-element',
+      name: 'div',
+      props: {
+        foo: {
+          type: NodePropType.APIResultProperty,
+          fallback: fallback,
+          stateID: 'some_api_state',
+        },
+      },
+    };
 
-  // })
+    const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: undefined,
+        error: new Error('should be handled'),
+        loading: false,
+      });
+    });
 
-  // test('when_api_return_error', () => {
+    // expect return fallback defined in schema
+    expect(result.current.foo).toBe(fallback);
+    expect(result.all.length).toBe(2);
 
-  // })
+    // expect return latest not-nullish fallback
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: 'some_new_data',
+        error: undefined,
+        loading: false,
+      });
 
-  // test('when_adapter_throw', () => {
+      apiStateHub.getState('some_api_state').next({
+        data: undefined,
+        error: new Error('should be handled'),
+        loading: false,
+      });
+    });
 
-  // })
+    // expect return fallback defined in schema
+    expect(result.current.foo).toBe('some_new_data');
+    // the last call of next will not resolve new result
+    // so result.all.length is 3
+    expect(result.all.length).toBe(3);
 
-  // test('when_adapter_return_undefined', () => {
+    unmount();
+  });
 
-  // })
+  test('when_adapter_throw', () => {
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
+    const fallback = { foo: 123 };
+    const node: SchemaNode<Instantiated> = {
+      key: 'some_key',
+      type: 'html-element',
+      name: 'div',
+      props: {
+        foo: {
+          type: NodePropType.APIResultProperty,
+          fallback: fallback,
+          stateID: 'some_api_state',
+          adapter: (shouldThrow: boolean) => {
+            if (shouldThrow) {
+              throw new Error('should be handled');
+            }
 
-  test('resolve_fallback_when_adapter_throw', () => {
+            return shouldThrow;
+          },
+        },
+      },
+    };
+
     // eslint-disable-next-line no-console
     console.error = jest.fn();
+    const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: true,
+        error: undefined,
+        loading: false,
+      });
+    });
+
+    // expect return fallback defined in schema
+    expect(result.current.foo).toEqual(fallback);
+    expect(result.all.length).toBe(2);
+
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: false,
+        error: undefined,
+        loading: false,
+      });
+
+      apiStateHub.getState('some_api_state').next({
+        data: true,
+        error: undefined,
+        loading: false,
+      });
+    });
+
+    expect(result.current.foo).toEqual(false);
+
+    // eslint-disable-next-line no-console
+    expect(console.error).toBeCalled();
+
+    unmount();
+  });
+
+  test('when_adapter_return_undefined', () => {
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
     const fallback = { foo: 123 };
     const node: SchemaNode<Instantiated> = {
       key: 'some_key',
@@ -72,27 +171,203 @@ describe('useAPIResultProps_resolve_expected_fallback', () => {
           fallback: fallback,
           stateID: 'some_api_state',
           adapter: () => {
-            throw new Error('should_be_handled');
+            return;
           },
         },
       },
     };
 
     const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: { foo: 'bar' },
+        error: undefined,
+        loading: false,
+      });
+    });
+
     expect(result.current.foo).toMatchObject(fallback);
-    // eslint-disable-next-line no-console
-    expect(console.error).toBeCalled();
+    expect(result.all.length).toBe(2);
 
     unmount();
   });
 
-  // 如果要使用 loading，应该是直接 state.loading 就可以了，
-  // 如果要使用 error，应该是直接 state.error 就可以了，
-  // 如果根据 loading 状态的不同返回不同的 组件 props value，那这个逻辑应该写在组件内部
-  // test('resolve_fallback_when_api_error', () => {
+  test('when_data_is_undefined', () => {
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
+    const fallback = { foo: 123 };
+    const node: SchemaNode<Instantiated> = {
+      key: 'some_key',
+      type: 'html-element',
+      name: 'div',
+      props: {
+        foo: {
+          type: NodePropType.APIResultProperty,
+          fallback: fallback,
+          stateID: 'some_api_state',
+        },
+      },
+    };
 
-  // })
+    const latestFallback = { bar: 'baz' };
+    const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: latestFallback,
+        error: undefined,
+        loading: false,
+      });
+      apiStateHub.getState('some_api_state').next({
+        data: undefined,
+        error: undefined,
+        loading: false,
+      });
+    });
+
+    expect(result.current.foo).toEqual(latestFallback);
+
+    unmount();
+  });
 });
+
+describe('useAPIResultProps_should_call_adapter_correctly', () => {
+  test('do_not_call_on_initial_stage', () => {
+    const adapter = jest.fn();
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
+    const fallback = { foo: 123 };
+    const node: SchemaNode<Instantiated> = {
+      key: 'some_key',
+      type: 'html-element',
+      name: 'div',
+      props: {
+        foo: {
+          type: NodePropType.APIResultProperty,
+          fallback: fallback,
+          stateID: 'some_api_state',
+          adapter: adapter,
+        },
+      },
+    };
+
+    const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+
+    expect(result.current.foo).toEqual(fallback);
+    expect(adapter).not.toBeCalled();
+
+    unmount();
+  });
+
+  test('do_not_call_when_data_is_undefined', () => {
+    const adapter = jest.fn();
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
+    const fallback = { foo: 123 };
+    const node: SchemaNode<Instantiated> = {
+      key: 'some_key',
+      type: 'html-element',
+      name: 'div',
+      props: {
+        foo: {
+          type: NodePropType.APIResultProperty,
+          fallback: fallback,
+          stateID: 'some_api_state',
+          adapter: adapter,
+        },
+      },
+    };
+
+    const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: undefined,
+        error: undefined,
+        loading: false,
+      });
+    });
+
+    expect(result.current.foo).toEqual(fallback);
+    expect(adapter).not.toBeCalled();
+
+    unmount();
+  });
+
+  test('do_call_when_data_is_not_nullish', () => {
+    const adapter = jest.fn((v) => v);
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
+    const fallback = { foo: 123 };
+    const node: SchemaNode<Instantiated> = {
+      key: 'some_key',
+      type: 'html-element',
+      name: 'div',
+      props: {
+        foo: {
+          type: NodePropType.APIResultProperty,
+          fallback: fallback,
+          stateID: 'some_api_state',
+          adapter: adapter,
+        },
+      },
+    };
+
+    const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: { bar: 'baz' },
+        error: undefined,
+        loading: false,
+      });
+    });
+
+    expect(result.current.foo).toEqual({ bar: 'baz' });
+    expect(adapter).toBeCalledWith({ bar: 'baz' });
+
+    unmount();
+  });
+
+  test('return_expected_value', () => {
+    const apiStateHub = new APIStateHub(builder, stateIDMap);
+    dummyCTX.apiStates = apiStateHub;
+    const fallback = { foo: 123 };
+    const node: SchemaNode<Instantiated> = {
+      key: 'some_key',
+      type: 'html-element',
+      name: 'div',
+      props: {
+        foo: {
+          type: NodePropType.APIResultProperty,
+          fallback: fallback,
+          stateID: 'some_api_state',
+          adapter: (v) => v * 2,
+        },
+      },
+    };
+
+    const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
+
+    act(() => {
+      apiStateHub.getState('some_api_state').next({
+        data: 2,
+        error: undefined,
+        loading: false,
+      });
+    });
+
+    expect(result.current.foo).toEqual(4);
+
+    unmount();
+  });
+});
+
+// 如果要使用 loading，应该是直接 state.loading 就可以了，
+// 如果要使用 error，应该是直接 state.error 就可以了，
+// 如果根据 loading 状态的不同返回不同的 组件 props value，那这个逻辑应该写在组件内部
+// test('resolve_fallback_when_api_error', () => {
+
+// })
 
 // describe('useAPIResultProps_resolve_expected_data_when_api_state_has_error', () => {
 //   test('return_fallback_when_previous_data_is_undefined', () => {
@@ -103,40 +378,3 @@ describe('useAPIResultProps_resolve_expected_fallback', () => {
 
 //   });
 // });
-
-test('expect_resolve_initial_value', () => {
-  const stateIDMap = {
-    some_api_state: { apiID: 'findPetsByTags' },
-  };
-  const apiStateHub = new APIStateHub(builder, stateIDMap);
-  dummyCTX.apiStates = apiStateHub;
-
-  const convertorFn = jest.fn();
-
-  const node: SchemaNode<Instantiated> = {
-    key: 'some_key',
-    type: 'html-element',
-    name: 'div',
-    props: {
-      foo: {
-        type: NodePropType.APIResultProperty,
-        fallback: { foo: 123 },
-        stateID: 'some_api_state',
-        adapter: convertorFn,
-      },
-      bar: {
-        type: NodePropType.APIResultProperty,
-        fallback: { bar: 456 },
-        stateID: 'some_api_state',
-        adapter: convertorFn,
-      },
-    },
-  };
-
-  const { result, unmount } = renderHook(() => useAPIResultProps(node, dummyCTX));
-  expect(result.current).toMatchObject({ foo: { foo: 123 }, bar: { bar: 456 } });
-  expect(result.all.length).toBe(1);
-  expect(convertorFn).not.toBeCalled();
-
-  unmount();
-});
