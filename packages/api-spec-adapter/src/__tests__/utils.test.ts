2a7bdfa06aae9c8926505dd70f4f167b22b87af2
Mark <markduan@yunify.com>
2021-11-19 17:02:00 +0800

    test: Add test case for util method join

---
+++ packages/api-spec-adapter/src/__tests__/utils.test.ts
@@ -0,0 +1,30 @@
+import { join } from '../utils';
+
+const cases: Array<{ segments: string[]; result: string; }> = [
+  {
+    segments: ['abc', 'def'],
+    result: '/abc/def',
+  },
+  {
+    segments: ['/abc', '/def'],
+    result: '/abc/def',
+  },
+  {
+    segments: ['/abc', 'def'],
+    result: '/abc/def',
+  },
+  {
+    segments: ['/abc', '/def/ghi'],
+    result: '/abc/def/ghi',
+  },
+  {
+    segments: ['/abc', '/def/ghi/'],
+    result: '/abc/def/ghi',
+  },
+];
+
+test('join_should_return_expected_value', () => {
+  cases.forEach(({ segments, result }) => {
+    expect(join(...segments)).toBe(result);
+  });
+});
