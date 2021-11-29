4661c08d15e65b3f665c9132512be29814fab279
Mark <markduan@yunify.com>
2021-11-29 15:10:08 +0800

    chore: Update example

--- packages/example/index.ts
+++ packages/example/index.ts
@@ -1,17 +1,13 @@
-import renderEngine, { RenderEngineInstance } from '@ofa/render-engine';
+import RenderEngine from '@ofa/render-engine';
 import { SwaggerSpecAdapter } from '@ofa/api-spec-adapter';
 
 import schema from './serialized-schema';
 import apiDoc from './api-doc';
 
-declare global {
-  interface Window {
-    renderEngineInstance: RenderEngineInstance;
-  }
-}
 const apiSpecAdapter = new SwaggerSpecAdapter(apiDoc);
 const renderRoot = document.querySelector('#react-root');
 
+const renderEngine = new RenderEngine({ schema, apiSpecAdapter });
 if (renderRoot) {
-  window.renderEngineInstance = renderEngine({ schema, apiSpecAdapter, renderRoot });
+  renderEngine.render(renderRoot);
 }
