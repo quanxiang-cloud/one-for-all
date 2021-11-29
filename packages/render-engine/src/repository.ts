12b2c341caca1992ad9aa5251e5d0ea4dc85fd93
Mark <markduan@yunify.com>
2021-11-29 16:26:13 +0800

    refactor: Delete useless code

--- packages/render-engine/src/repository.ts
+++ packages/render-engine/src/repository.ts
@@ -1,4 +1,4 @@
-import { DynamicComponent } from './types';
+import type { DynamicComponent } from './types';
 
 type ComponentURLResolver = (componentName: string, version?: string) => string;
 // TODO: support import const value and prune functions
@@ -21,51 +21,17 @@ export function register(nameSpace: string, options: RegistryOptions): void {
   repository[nameSpace] = options;
 }
 
-// function resolveComponentPath(componentName: string, version: string) {
-//   // render import-map.json
-//   return `/packages/${componentName}/dist/index.js`;
-// }
-
 type ImportComponentParams = {
   packageName: string;
   version: string;
   exportName?: string;
 }
 
+// todo how to concat packageName and packageVersion?
 export function importComponent(
   { packageName, exportName }: ImportComponentParams,
 ): Promise<DynamicComponent> {
   return System.import(packageName).then((systemModule) => {
     return systemModule[exportName || 'default'];
   });
-}
-
-// TODO: component name should be lower case
-export function getBasicComponentsOptions(): RegistryOptions {
-  return {
-    componentURLResolver: (_, version): string => {
-      return `/dist/bundle@${version}.js`;
-    },
-    importer: (systemModule, componentName): DynamicComponent => {
-      // TODO: check module is undefined
-      return systemModule[componentName];
-    },
-  };
-}
-
-export function getAdvancedComponentsOptions(): RegistryOptions {
-  return {
-    componentURLResolver: (componentName, version): string => {
-      const fragments = componentName.split('/');
-
-      if (fragments.length === 1) {
-        return `/dist/advanced-components/${fragments[0]}@${version}/index.js`;
-      }
-
-      const mainComponentName = fragments.shift();
-      fragments.unshift(`${mainComponentName}@${version}`);
-
-      return `/dist/advanced-components/${fragments.join('/')}.js`;
-    },
-  };
 }
