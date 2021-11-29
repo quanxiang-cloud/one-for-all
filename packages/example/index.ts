baf3a6b423c4b9fcb866f63e46a6dae19338bed6
Mark <markduan@yunify.com>
2021-11-29 15:15:37 +0800

    refactor: Add SchemaRender example

--- packages/example/index.ts
+++ packages/example/index.ts
@@ -1,4 +1,6 @@
-import RenderEngine from '@ofa/render-engine';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RenderEngine, { SchemaRender } from '@ofa/render-engine';
 import { SwaggerSpecAdapter } from '@ofa/api-spec-adapter';
 
 import schema from './serialized-schema';
@@ -6,8 +8,11 @@ import apiDoc from './api-doc';
 
 const apiSpecAdapter = new SwaggerSpecAdapter(apiDoc);
 const renderRoot = document.querySelector('#react-root');
+const schemaRenderRoot = document.querySelector('#schema-react-root');
 
 const renderEngine = new RenderEngine({ schema, apiSpecAdapter });
 if (renderRoot) {
   renderEngine.render(renderRoot);
 }
+
+ReactDOM.render(React.createElement(SchemaRender, { schema, apiSpecAdapter }), schemaRenderRoot);
