7fa82f9f3014bc6927a8639102397811f5cf5448
Mark <markduan@yunify.com>
2021-12-02 17:45:54 +0800

    chore: Bundle output support latest version

--- scripts/get-common-output.js
+++ scripts/get-common-output.js
@@ -1,14 +1,25 @@
 import { terser } from 'rollup-plugin-terser';
 
-export default function getOutput(pkgName) {
+export default function getOutput(pkgName, pkgVersion) {
   return [
     {
-      file: `dist/${pkgName}/index.js`,
+      file: `dist/${pkgName}@${pkgVersion}/index.js`,
       format: 'system',
       sourcemap: 'inline',
     },
     {
-      file: `dist/${pkgName}/index.min.js`,
+      file: `dist/${pkgName}@${pkgVersion}/index.min.js`,
+      format: 'system',
+      sourcemap: false,
+      plugins: [terser()],
+    },
+    {
+      file: `dist/${pkgName}@latest/index.js`,
+      format: 'system',
+      sourcemap: 'inline',
+    },
+    {
+      file: `dist/${pkgName}@latest/index.min.js`,
       format: 'system',
       sourcemap: false,
       plugins: [terser()],
