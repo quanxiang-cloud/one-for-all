d689222dc25240e0e90edeb95734c438cf75a6d0
iriskang <iriskang@yunify.com>
2021-12-17 17:32:17 +0800

    feat: Add button in page-engine

---
+++ packages/page-engine/src/registry/basic/button/button.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Button } from '@ofa/ui';
+
+export interface Props {
+  title?: string;
+  size?: 'normal' | 'compact';
+  modifier?: 'primary' | 'danger';
+  loading?: boolean;
+  forbidden?: boolean;
+  iconName?: string;
+  iconSize?: number;
+  textClassName?: string;
+  iconClassName?: string;
+}
+
+function ButtonElem(props: Props): JSX.Element {
+  const { title = '按钮' } = props;
+  return (
+
+    <Button {...props}>{title}</Button>
+  );
+}
+
+export default ButtonElem;
+
