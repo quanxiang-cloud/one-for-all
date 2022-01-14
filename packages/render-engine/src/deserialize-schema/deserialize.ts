061d5b3fea52bb85ff999314c4f0f940154db177
Mark <markduan@yunify.com>
2022-01-14 13:03:38 +0800

    refactor: Rewrite deserialize schema function

---
+++ packages/render-engine/src/deserialize-schema/deserialize.ts
@@ -0,0 +1,30 @@
+import { CTX } from '../types';
+import { instantiateFuncSpec, isObject, isFuncSpec } from './utils';
+
+function deserialize(n: unknown, ctx: CTX): void {
+  if (!isObject(n) || typeof n !== 'object' || n === null) {
+    return;
+  }
+
+  Object.entries(n).forEach(([key, v]) => {
+    if (isFuncSpec(v)) {
+      Reflect.set(n, key, instantiateFuncSpec(v, ctx));
+      return;
+    }
+
+    if (isObject(v)) {
+      deserialize(v, ctx);
+      return;
+    }
+
+    if (Array.isArray(v)) {
+      Reflect.set(n, key, v.map((_v) => {
+        deserialize(_v, ctx);
+        return _v;
+      }));
+      return;
+    }
+  });
+}
+
+export default deserialize;
