396f77485fd0927070e988f6b79b2077d2ad589f
Mark <markduan@yunify.com>
2022-01-18 18:04:24 +0800

    chore: Move warn message into useEffect hook

--- packages/render-engine/src/use-instantiate-props/use-api-invoke-props.ts
+++ packages/render-engine/src/use-instantiate-props/use-api-invoke-props.ts
@@ -7,8 +7,6 @@ import { APIInvokeProperty, CTX, SchemaNode } from '../types';
 type APICallProps = Record<string, (...args: unknown[]) => void>;
 
 export default function useAPIInvokeProps(node: SchemaNode, ctx: CTX): APICallProps {
-  logger.warn('hook useAPIInvokeProps has been deprecated, please use hook useFuncProps instead');
-
   return useMemo(() => {
     if (!node.props) {
       return {};
@@ -17,6 +15,8 @@ export default function useAPIInvokeProps(node: SchemaNode, ctx: CTX): APICallPr
     return Object.entries(node.props).filter((pair): pair is [string, APIInvokeProperty] => {
       return pair[1].type === 'api_invoke_property';
     }).reduce<APICallProps>((acc, [propName, { stateID, paramsBuilder, callback }]) => {
+      logger.warn('hook useAPIInvokeProps has been deprecated, please use hook useFuncProps instead');
+
       function handleAction(...args: unknown[]): void {
         try {
           const fetchParams: FetchParams = paramsBuilder?.(...args) || {};
