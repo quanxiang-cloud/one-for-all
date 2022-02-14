9a334747055d4b45bf3523a7cededc34fd53d095
Mark <markduan@yunify.com>
2022-02-14 11:41:22 +0800

    refactor: Deserialize schema in constructor

--- packages/render-engine/src/render-engine.ts
+++ packages/render-engine/src/render-engine.ts
@@ -5,25 +5,26 @@ import type { Schema } from '@one-for-all/schema-spec';
 import deserializeSchema from './deserialize-schema';
 import NodeRender from './node-render';
 import initCTX from './ctx';
-import type { InitProps, CTX } from './types';
+import type { InitProps, CTX, SchemaNode } from './types';
 
 export default class RenderEngine {
   ctx: CTX;
   schema: Schema;
+  node: SchemaNode | null;
 
   constructor(initProps: InitProps) {
     this.schema = initProps.schema;
     this.ctx = initCTX(initProps);
+    this.node = deserializeSchema(this.schema.node, this.ctx);
   }
 
   render(renderRoot: Element): void {
-    const instantiatedNode = deserializeSchema(this.schema.node, this.ctx);
-    if (!instantiatedNode) {
+    if (!this.node) {
       // TODO: paint error
       // return { ctx };
       return;
     }
 
-    ReactDOM.render(React.createElement(NodeRender, { node: instantiatedNode, ctx: this.ctx }), renderRoot);
+    ReactDOM.render(React.createElement(NodeRender, { node: this.node, ctx: this.ctx }), renderRoot);
   }
 }
