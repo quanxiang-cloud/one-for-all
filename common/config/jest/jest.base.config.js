f245767c0940367a877c32235b0c090991b31914
Mark <markduan@yunify.com>
2022-02-15 18:16:27 +0800

    fix: Failed to load jest type definiation

--- common/config/jest/jest.base.config.js
+++ common/config/jest/jest.base.config.js
@@ -24,5 +24,5 @@ export default {
   testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ['__tests__/fixtures'],
   // moduleNameMapper: mapper,
-  setupFilesAfterEnv: [path.resolve(__dirname, './jest-setup-import-jest-dom.ts')]
+  setupFilesAfterEnv: [path.resolve(__dirname, './jest-setup-import-jest-dom.js')]
 };
