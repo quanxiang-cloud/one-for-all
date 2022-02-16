7158a33b909c8d50bc15e2277a37ae4a44f4821e
Mark <markduan@yunify.com>
2022-02-16 13:31:27 +0800

    chore: Remove useless code in jest config

--- common/config/jest/jest.base.config.js
+++ common/config/jest/jest.base.config.js
@@ -1,19 +1,8 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import { lstatSync, readdirSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// const basePath = path.resolve(__dirname, 'packages');
-// const packages = readdirSync(basePath).filter((name) => {
-//   return lstatSync(path.join(basePath, name)).isDirectory();
-// });
-
-// const mapper = packages.reduce((mapper, name) => {
-//   mapper[`@one-for-all/${name}`] = path.join(basePath, name, 'src');
-//   return mapper;
-// }, {});
 
 export default {
   preset: 'ts-jest',
@@ -23,6 +12,5 @@ export default {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ['__tests__/fixtures'],
-  // moduleNameMapper: mapper,
   setupFilesAfterEnv: [path.resolve(__dirname, './jest-setup-import-jest-dom.js')]
 };
