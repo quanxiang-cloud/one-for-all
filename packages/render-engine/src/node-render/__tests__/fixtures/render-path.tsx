1223f06a6d1b6bf8125cdbbad5ecfe853d7cef7e
Mark <markduan@yunify.com>
2022-02-16 13:24:51 +0800

    test: Add test case about node path

---
+++ packages/render-engine/src/node-render/__tests__/fixtures/render-path.tsx
@@ -0,0 +1,19 @@
+import React, { PropsWithChildren, useContext } from 'react';
+import { Repository } from '../../..';
+import PathContext from '../../path-context';
+
+type Props = PropsWithChildren<{
+  id: string;
+}>;
+
+function RenderPath({ id, children }: Props): JSX.Element {
+  const parentPath = useContext(PathContext)
+
+  return (<div id={id} data-path={parentPath}>{children}</div>);
+}
+
+const repository: Repository = {
+  'PathContextTest@whatever': { RenderPath },
+};
+
+export default repository;
