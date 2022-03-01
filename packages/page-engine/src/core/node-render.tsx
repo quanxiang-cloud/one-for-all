0d2214e36e1886d657c964d3d57d90a1ffdf6357
sunnyw <sunnywang@yunify.com>
2022-03-01 10:39:44 +0800

    fix: Container placeholder height auto fit
    see: #lc-2389

--- packages/page-engine/src/core/node-render.tsx
+++ packages/page-engine/src/core/node-render.tsx
@@ -161,13 +161,15 @@ function NodeRender({ schema }: Props): JSX.Element | null {
         Object.assign(elemProps, {
           placeholder: (
             <div
-              className={cs('border border-dashed flex items-center justify-center', {
+              className={cs('border border-dashed flex items-center justify-center w-full h-full', {
                 'bg-gray-100': !backgroundColor && !backgroundImage,
               })}
-              style={{ minHeight: (heightValue.value && Number(heightValue.value) < 60 &&
-                heightValue.unit === 'px') ? Number(heightValue.value) : 60,
-              backgroundColor: backgroundColor,
-              backgroundImage: backgroundImage }}
+              style={{
+                minHeight: (heightValue.value && Number(heightValue.value) < 60 &&
+                  heightValue.unit === 'px') ? Number(heightValue.value) : 60,
+                backgroundColor: backgroundColor,
+                backgroundImage: backgroundImage
+              }}
             >
               {(heightValue.value && Number(heightValue.value) < 60 && heightValue.unit === 'px') ?
                 '' : COMP_WITH_PLACEHOLDER[schema.exportName]}
