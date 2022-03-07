49941d83a7d024d6d54859504173116d7904b835
tanjie <starrytan@yunify.com>
2022-03-07 16:36:05 +0800

    feat: Add component style config page

--- packages/headless-ui/src/web/index.ts
+++ packages/headless-ui/src/web/index.ts
@@ -3,6 +3,8 @@ import Checkbox, { CheckboxGroup } from '../shared/checkbox';
 import Input from '../shared/input';
 import Divider from '../shared/divider';
 import Tag from '../shared/tag';
+import Button from '../shared/button';
+import Table from '../shared/table';
 import Breadcrumb from '../shared/breadcrumb';
 
 export {
@@ -14,4 +16,6 @@ export {
   Divider,
   Tag,
   Breadcrumb,
+  Button,
+  Table
 };
