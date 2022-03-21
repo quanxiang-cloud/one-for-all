b54b2a2286108c39596b23e7e8a84ea84954ee2c
Mark <dgwhangzhou@gmail.com>
2022-03-21 13:29:31 +0800

    fix: Missing export of findNodeByID

--- packages/schema-utils/src/index.js
+++ packages/schema-utils/src/index.js
@@ -1,6 +1,7 @@
 export * from './raw/all';
 export { default as appendChild } from './appendChild';
 export { default as deleteByID } from './deleteByID';
+export { default as findNodeByID } from './findNodeByID';
 export { default as getNodeParentIDs } from './getNodeParentIDs';
 export { default as getNodeParents } from './getNodeParents';
 export { default as patchNode } from './patchNode';
