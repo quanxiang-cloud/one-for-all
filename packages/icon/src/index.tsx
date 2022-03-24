1200f77c219cc94fe2fa7347655ef3ae1bc93274
mingbing737 <81600220+mingbing737@users.noreply.github.com>
2022-03-24 11:35:18 +0800

    refactor: Improve the icon component library (#36)
    1、Remove multi-color icon
    2、Add the json file of the name of the icon
    3、Rewrite the icon preview page

--- packages/icon/src/index.tsx
+++ packages/icon/src/index.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-
 import svgSprite from './sprite.svg';
 
 interface IconProps {
   name: string;
   className?: string;
   size?: number | string;
+  color?: string;
   style?: React.CSSProperties;
 }
 
 function Icon(
-  { name, size = 16, className = '', style }: IconProps,
+  { name, size = 16, color, className = '', style }: IconProps,
   ref?: React.Ref<SVGSVGElement>,
 ): JSX.Element {
   const _style: React.CSSProperties = {
     ...style,
     width: typeof size === 'string' ? size : `${size}px`,
     height: typeof size === 'string' ? size : `${size}px`,
+    fill: color,
   };
-
 
   return (
     <svg ref={ref} data-name={name} style={_style} className={'ofa-svg-icon ' + className}>
