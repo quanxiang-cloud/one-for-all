5d9fd0fd34184881ef136b5d5321a5dda98927d6
Bunny miao <49241072+hhhqm@users.noreply.github.com>
2022-04-01 09:22:27 +0800

    feat: Feat UI route link (#44)

--- packages/example/src/todo-app/repository.ts
+++ packages/example/src/todo-app/repository.ts
@@ -1,7 +1,8 @@
+import { Repository } from '@one-for-all/render-engine';
+
 import TodoList from './components/todo-list';
 import TodoInput from './components/todo-input';
 import TodoFilter from './components/todo-filter';
-import { Repository } from '@one-for-all/render-engine';
 
 const todoAppRepository: Repository = {
   'todo-app@whatever': {
