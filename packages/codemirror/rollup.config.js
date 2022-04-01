2b4f3767c171a14297c5551fdb6c84133fe42696
tanjie <starrytan@yunify.com>
2022-04-01 17:12:28 +0800

    feat: Add codemirror package

---
+++ packages/codemirror/rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve';
+import styles from 'rollup-plugin-styles';
+import commonjs from '@rollup/plugin-commonjs';
+import getOutput from '../../common/config/rollup/get-common-output';
+import packageJSON from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: getOutput(packageJSON.name, packageJSON.version),
+  external: ['react', 'react-dom'],
+  plugins: [
+    commonjs(),
+    styles({ modules: false }),
+    resolve({
+      preferBuiltins: false,
+      browser: true,
+      mainFields: ['module', 'main'],
+    }),
+  ],
+};
