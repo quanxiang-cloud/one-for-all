7d552524a8213d6e2b7c3c4c9f00c7501f8316e9
lishengma <lishengma@yunify.com>
2022-05-12 17:20:42 +0800

    fix: Package version

--- packages/ui/config/index.ts
+++ packages/ui/config/index.ts
@@ -3,14 +3,14 @@ import propsSpec from './props-spec.json';
 
 export const params = [{
   key: 'PACKAGE_MANIFEST:@one-for-all/ui',
-  version: 'latest',
+  version: '0.3.3',
   value: JSON.stringify(manifest)
 }]
 // __httpClient('/api/v1/persona/batchSetValue', { keys: params });
 
 export const specParams = [{
   key: 'PACKAGE_PROPS_SPEC:@one-for-all/ui',
-  version: 'latest',
+  version: '0.3.3',
   value: JSON.stringify(propsSpec),
 }]
 // __httpClient('/api/v1/persona/batchSetValue', { keys: specParams });
