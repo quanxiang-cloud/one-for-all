1f96be85d477a19fe751933c9be7f9ebb69e125b
Mark <dgwhangzhou@gmail.com>
2022-05-18 14:07:49 +0800

    style: Update container children placeholder style

--- packages/artery-simulator/src/simulator/background/node-render/placeholder.tsx
+++ packages/artery-simulator/src/simulator/background/node-render/placeholder.tsx
@@ -1,32 +1,32 @@
-import type { HTMLNode, ReactComponentNode } from '@one-for-all/artery-renderer';
 import React from 'react';
-import { NodePrimary } from '../../../types';
+// import type { HTMLNode, ReactComponentNode } from '@one-for-all/artery-renderer';
+// import { NodePrimary } from '../../../types';
 
-interface Props {
-  parent: HTMLNode | ReactComponentNode;
-}
+// interface Props {
+//   parentNode: HTMLNode | ReactComponentNode;
+// }
 
-export function getParentNode(parent: HTMLNode | ReactComponentNode): NodePrimary {
-  if (parent.type === 'html-element') {
-    return { type: 'html-element', name: parent.name };
-  }
+// export function getParentNode(parent: HTMLNode | ReactComponentNode): NodePrimary {
+//   if (parent.type === 'html-element') {
+//     return { type: 'html-element', name: parent.name };
+//   }
 
-  return {
-    type: 'react-component',
-    packageName: parent.packageName,
-    packageVersion: parent.packageVersion,
-    exportName: parent.exportName,
-  };
-}
+//   return {
+//     type: 'react-component',
+//     packageName: parent.packageName,
+//     packageVersion: parent.packageVersion,
+//     exportName: parent.exportName,
+//   };
+// }
 
 function EmptyPlaceholder(): JSX.Element {
   return <div>请拖拽组件到此处！</div>;
 }
 
-function Placeholder({ parent }: Props): JSX.Element | null {
+function Placeholder(): JSX.Element | null {
   return React.createElement(
     'div',
-    { className: 'placeholder-for-empty-children' },
+    { className: 'placeholder-for-container-node-children' },
     React.createElement(EmptyPlaceholder),
   );
 }
