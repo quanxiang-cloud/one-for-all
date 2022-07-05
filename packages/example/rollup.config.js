461358aee9dba845dc7b8f42c097b24cf0ae4829
Mark <dgwhangzhou@gmail.com>
2022-07-05 18:03:07 +0800

    fix: Example rollup config

--- packages/example/rollup.config.js
+++ packages/example/rollup.config.js
@@ -4,65 +4,66 @@ import styles from 'rollup-plugin-styles';
 import esbuild from 'rollup-plugin-esbuild';
 import html from '@rollup/plugin-html';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 import typescriptPaths from '../../scripts/rollup-plugin-typescript-paths';
 
 export default [
-  // {
-  //   input: {
-  //     index: 'src/index.ts',
-  //     outline: 'src/outline.tsx',
-  //     temporaryPlugins: 'src/artery-simulator/temporary-plugins.ts',
-  //   },
-  //   output: {
-  //     dir: 'dist',
-  //     format: 'system',
-  //     sourcemap: 'inline',
-  //   },
+  {
+    input: {
+      index: 'src/index.ts',
+      outline: 'src/outline.tsx',
+      temporaryPlugins: 'src/artery-simulator/temporary-plugins.ts',
+    },
+    output: {
+      dir: 'dist',
+      format: 'system',
+      sourcemap: 'inline',
+    },
 
-  //   external: ['react', 'react-dom', 'lodash', /@one-for-all\/.*/],
+    external: ['react', 'react-dom', 'lodash', /@one-for-all\/.*/],
 
-  //   plugins: [
-  //     commonjs(),
-  //     styles({ modules: false }),
-  //     nodeResolve({
-  //       browser: true,
-  //       mainFields: ['main'],
-  //     }),
-  //     typescriptPaths(),
-  //     esbuild({
-  //       // All options are optional
-  //       include: /\.[jt]sx?$/, // default, inferred from `loaders` option
-  //       exclude: /node_modules/, // default
-  //       sourceMap: true,
-  //       minify: process.env.NODE_ENV === 'production',
-  //       target: 'es2017', // default, or 'es20XX', 'esnext'
-  //       jsx: 'transform', // default, or 'preserve'
-  //       jsxFactory: 'React.createElement',
-  //       jsxFragment: 'React.Fragment',
-  //       // Like @rollup/plugin-replace
-  //       define: {
-  //         __VERSION__: '"x.y.z"',
-  //       },
-  //       tsconfig: 'tsconfig.json', // default
-  //       // Add extra loaders
-  //       loaders: {
-  //         // Add .json files support
-  //         // require @rollup/plugin-commonjs
-  //         '.json': 'json',
-  //         // Enable JSX in .js files too
-  //         '.js': 'jsx',
-  //       },
-  //     }),
-  //   ],
-  // },
+    plugins: [
+      commonjs(),
+      styles({ modules: false }),
+      nodeResolve({
+        browser: true,
+        mainFields: ['main'],
+      }),
+      typescriptPaths(),
+      esbuild({
+        // All options are optional
+        include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+        exclude: /node_modules/, // default
+        sourceMap: true,
+        minify: process.env.NODE_ENV === 'production',
+        target: 'es2017', // default, or 'es20XX', 'esnext'
+        jsx: 'transform', // default, or 'preserve'
+        jsxFactory: 'React.createElement',
+        jsxFragment: 'React.Fragment',
+        // Like @rollup/plugin-replace
+        define: {
+          __VERSION__: '"x.y.z"',
+        },
+        tsconfig: 'tsconfig.json', // default
+        // Add extra loaders
+        loaders: {
+          // Add .json files support
+          // require @rollup/plugin-commonjs
+          '.json': 'json',
+          // Enable JSX in .js files too
+          '.js': 'jsx',
+        },
+      }),
+    ],
+  },
   // config for todo-app
   {
     input: 'src/todo-app/index.ts',
     output: {
       dir: 'dist/todo',
       format: 'system',
-      sourcemap: 'inline',
+      plugins: [terser()],
     },
 
     external: ['react', 'react-dom', 'lodash', /@one-for-all\/.*/],
