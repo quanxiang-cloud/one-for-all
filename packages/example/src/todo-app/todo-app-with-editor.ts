dd825fa1e92c9926f47097b887055855bd5a6dad
Mark <dgwhangzhou@gmail.com>
2022-07-18 16:57:51 +0800

    chore: Add description in todo demo

--- packages/example/src/todo-app/todo-app-with-editor.ts
+++ packages/example/src/todo-app/todo-app-with-editor.ts
@@ -38,21 +38,58 @@ const todoWithEditor: Artery = {
         ],
       },
       {
-        id: 'editor',
-        type: 'react-component',
-        packageName: 'todo-with-editor',
-        exportName: 'Editor',
-        packageVersion: 'whatever',
-        props: {
-          onChange: {
-            type: 'functional_property',
-            func: {
-              type: '',
-              args: 'v',
-              body: 'this.states.artery = v'
+        id: 'editor-wrapper',
+        type: 'html-element',
+        name: 'div',
+        children: [
+          {
+            id: 'desc',
+            type: 'html-element',
+            name: 'div',
+            props: {
+              children: {
+                type: 'constant_property',
+                value: '编辑下方 Artery，在左侧预览渲染结果。'
+              }
+            },
+          },
+          {
+            id: 'artery-spec-link',
+            type: 'html-element',
+            name: 'a',
+            props: {
+              target: {
+                type: 'constant_property',
+                value: '_blank'
+              },
+              href: {
+                type: 'constant_property',
+                value: 'https://github.com/quanxiang-cloud/one-for-all/tree/main/packages/artery',
+              },
+              children: {
+                type: 'constant_property',
+                value: '查看 Artery Spec'
+              }
+            },
+          },
+          {
+            id: 'editor',
+            type: 'react-component',
+            packageName: 'todo-with-editor',
+            exportName: 'Editor',
+            packageVersion: 'whatever',
+            props: {
+              onChange: {
+                type: 'functional_property',
+                func: {
+                  type: '',
+                  args: 'v',
+                  body: 'this.states.artery = v'
+                }
+              }
             }
-          }
-        }
+          },
+        ]
       },
     ]
   }
